39291d8a35768567d20b9079df7f8aee272ac4d4
james-kaddissi <jameskaddissi@gmail.com>
2024-06-25 20:39:29 -0400

    Added a color picker widget that allows you to view and copy and color from the screen quickly

--- src/gsedit/sidebar.py
+++ src/gsedit/sidebar.py
@@ -4,11 +4,59 @@ from PyQt5.Qsci import *
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
 
+from pygame import Color
 from qframelesswindow import FramelessMainWindow, TitleBar
 from gsedit.top_bar import TopBarSmall
 import gsedit.theme_editor
 import os
 import gsedit.gsconfig
+class ColorPickerButton(QWidget):
+    def __init__(self, parent=None):
+        super(ColorPickerButton, self).__init__(parent)
+        self.init_ui()
+
+    def init_ui(self):
+        self.color_label = QLabel(self)
+        self.color_label.setText("Color Picker")
+        self.color_label.setStyleSheet("background-color: red; color: white;")
+        self.color_label.setAlignment(Qt.AlignCenter)
+        self.color_label.setFixedSize(70, 30)
+        font_size = 7 
+        font = QFont()
+        font.setPointSize(font_size)
+        self.color_label.setFont(font)
+        
+        self.color_label.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+        
+        self.color_label.mousePressEvent = self.start_color_picker
+
+    def start_color_picker(self, event):
+        self.grabMouse()
+        self.setCursor(Qt.CrossCursor)
+        self.color_label.setText("Picking...")
+
+    def mouseMoveEvent(self, event):
+        screen = QApplication.primaryScreen()
+        color = screen.grabWindow(QApplication.desktop().winId(), event.globalX(), event.globalY(), 1, 1).toImage().pixel(0, 0)
+        color = QColor(color)
+        hex_color = color.name()
+        print(hex_color)
+        self.color_label.setStyleSheet(f"background-color: {hex_color}; color: white; padding: 5px;")
+        self.color_label.setText(hex_color)
+        self.update()
+
+    def mousePressEvent(self, event):
+        screen = QApplication.primaryScreen()
+        color = screen.grabWindow(QApplication.desktop().winId(), event.globalX(), event.globalY(), 1, 1).toImage().pixel(0, 0)
+        color = QColor(color)
+        hex_color = color.name()
+        clipboard = QApplication.clipboard()
+        clipboard.setText(hex_color)
+        self.releaseMouse()
+        self.setCursor(Qt.ArrowCursor)
+        self.color_label.setText(f"Copied: {hex_color}")
+        self.color_label.setStyleSheet(f"background-color: {hex_color}; color: white;")
+
 class CustomTitleBar(TitleBar):
     def __init__(self, parent):
         super().__init__(parent)
@@ -47,6 +95,7 @@ class CustomTitleBar(TitleBar):
                 qproperty-pressedBackgroundColor: rgb(54, 57, 65);
             }
         """)
+
 class ThemeInfoWidget(QWidget):
     def __init__(self, theme_data, parent=None):
         super(ThemeInfoWidget, self).__init__(parent)
@@ -87,39 +136,6 @@ class ThemeInfoWidget(QWidget):
                 layout.addWidget(syntax_label)
         
         self.setLayout(layout)
-class ColorPickerButton(QWidget):
-    def __init__(self, parent=None):
-        super(ColorPickerButton, self).__init__(parent)
-        self.color_label = QLabel(self)
-        self.color_label.setText("Color Picker")
-        self.color_label.setStyleSheet("background-color: gray; color: white; padding: 5px;")
-        self.color_label.setAlignment(Qt.AlignCenter)
-        self.color_label.setGeometry(10, 550, 100, 40)
-
-        self.color_label.mousePressEvent = self.start_color_picker
-
-    def start_color_picker(self, event):
-        self.grabMouse()
-        self.setCursor(Qt.CrossCursor)
-        self.color_label.setText("Picking...")
-
-    def mouseMoveEvent(self, event):
-        color = QPixmap.grabWindow(QApplication.desktop().winId(), event.globalX(), event.globalY(), 1, 1).toImage().pixel(0, 0)
-        color = QColor(color)
-        hex_color = color.name()
-        self.color_label.setStyleSheet(f"background-color: {hex_color}; color: white; padding: 5px;")
-        self.color_label.setText(hex_color)
-        self.update()
-
-    def mousePressEvent(self, event):
-        color = QPixmap.grabWindow(QApplication.desktop().winId(), event.globalX(), event.globalY(), 1, 1).toImage().pixel(0, 0)
-        color = QColor(color)
-        hex_color = color.name()
-        clipboard = QApplication.clipboard()
-        clipboard.setText(hex_color)
-        self.releaseMouse()
-        self.setCursor(Qt.ArrowCursor)
-        self.color_label.setText(f"Copied: {hex_color}")
 
 class ThemeEditor(FramelessMainWindow):
     def __init__(self, parent=None):
@@ -129,22 +145,31 @@ class ThemeEditor(FramelessMainWindow):
         self.mwindow = parent
         self.title_bar = CustomTitleBar(self)
         self.setTitleBar(self.title_bar)
-        self.setWindowTitle("Theme Editor")
+        
         self.main_layout = QVBoxLayout()
         self.main_layout.setContentsMargins(0, 0, 0, 0)
-        self.main_layout.setSpacing(0)  
+        self.main_layout.setSpacing(0)
         
         main_theme_tab = self.mainTab()
         
         self.main_layout.addWidget(main_theme_tab)
+        
+        bottom_bar_layout = QHBoxLayout()
+        bottom_bar_layout.setContentsMargins(0, 0, 0, 0)
+        
+        self.color_picker = ColorPickerButton()
+        self.color_picker.setFixedSize(70, 30)
+        self.color_picker.setStyleSheet("""margin: 0px; padding: 0px""")
+        bottom_bar_layout.addWidget(self.color_picker, alignment=Qt.AlignBottom | Qt.AlignLeft)
+        self.main_layout.addLayout(bottom_bar_layout)
+        
         self.main_widget = QWidget()
         self.main_widget.setObjectName("mainWidget")
         self.main_widget.setLayout(self.main_layout)
         self.main_widget.setStyleSheet(self.refresh_style('mainWidgetThemeEditor'))
         self.setCentralWidget(self.main_widget)
         self.setStyleSheet(self.refresh_style('themeEditor'))
-        self.color_picker_button = ColorPickerButton(self.main_widget)
-        self.color_picker_button.setGeometry(10, self.height() - 50, 100, 40)
+
         self.titleBar.raise_()
         
     def mainTab(self):
@@ -160,7 +185,7 @@ class ThemeEditor(FramelessMainWindow):
         
         stack = QStackedWidget()
         stack.addWidget(ThemeInfoWidget(theme_data=gsedit.theme_editor.read_theme_file()))
-        stack.addWidget(QLabel("Customize Current Theme"))
+        stack.addWidget(ColorPickerButton())
         stack.addWidget(QLabel("Browse Themes"))
         
         main_theme_list.currentRowChanged.connect(stack.setCurrentIndex)
@@ -170,13 +195,12 @@ class ThemeEditor(FramelessMainWindow):
         
         tab.setLayout(tab_layout)
         return tab
+    
     def refresh_style(self, name):
         base_path = os.path.dirname(__file__)
         style_sheet_path = os.path.join(base_path, 'css', name+'.qss')
         with open(style_sheet_path, "r") as style_file:
             return style_file.read()
-    
-
 class Sidebar(QFrame):
     def __init__(self, window) -> None:
         super(Sidebar, self).__init__()
