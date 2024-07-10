6a882156621ff2fa7fc24d0079f0307118ba85ef
james-kaddissi <jameskaddissi@gmail.com>
2024-07-10 12:02:56 -0400

    Added the theme overview and syncThemes setting

--- src/gsedit/themes_editor.py
+++ src/gsedit/themes_editor.py
@@ -550,6 +550,67 @@ class LexerThemeInfoWidget(QWidget):
                 
                 layout.addWidget(syntax_label)
         
+        
+        self.setLayout(layout)
+
+    def refresh_style(self, name):
+        base_path = os.path.dirname(__file__)
+        style_sheet_path = os.path.join(base_path, 'css', name+'.qss')
+        with open(style_sheet_path, "r") as style_file:
+            return style_file.read()
+
+class ThemeInfoWidget(QWidget):
+    def __init__(self, lexer_theme_data, editor_theme_data, parent=None):
+        super(ThemeInfoWidget, self).__init__(parent)
+        self.window = parent
+        self.lexer_theme_data = lexer_theme_data
+        self.editor_theme_data = editor_theme_data
+        self.init_ui()
+        
+    def init_ui(self):
+        layout = QVBoxLayout()
+        if(gsedit.gsconfig.get_config("syncThemes")):
+            title_label = QLabel(f"Theme Name: {self.lexer_theme_data['active-theme']['theme-name']}")
+            title_label.setStyleSheet(self.refresh_style("lexerViewTitleLabel"))
+            version_label = QLabel(f"Version: {self.lexer_theme_data['active-theme']['version']}")
+            version_label.setStyleSheet(self.refresh_style("lexerViewVersionLabel"))
+            layout.addWidget(title_label)
+            layout.addWidget(version_label)
+        page_color = QColor(self.lexer_theme_data['active-theme']['syntax-rules'][0]['default']['page-color'])
+        self.setAutoFillBackground(True)
+        palette = self.palette()
+        palette.setColor(QPalette.Window, page_color)
+        self.setPalette(palette)
+        font_family = self.lexer_theme_data['active-theme']['syntax-rules'][0]['default']['font-family']
+        font_label = QLabel(f"Font Family: {font_family}")
+        font_label.setStyleSheet(self.refresh_style("lexerViewFontLabel"))
+        layout.addWidget(font_label)
+        for rule in self.lexer_theme_data['active-theme']['syntax-rules']:
+            for key, value in rule.items():
+                syntax_label = QLabel(f"{key.capitalize()}")
+                font = QFont()
+                font.setFamily(value['font-family'])
+                font.setPointSize(value['font-size'])
+                font.setWeight(QFont.Normal if value['font-weight'] == "normal" else QFont.Bold)
+                font.setItalic(value['italic'])
+                font.setUnderline(value['underlined'])
+                font.setStrikeOut(value['strikethrough'])
+                syntax_label.setFont(font)
+                text_color = QColor(value['text-color'])
+                syntax_label.setStyleSheet(f"color: {text_color.name()};")
+                layout.addWidget(syntax_label)
+        for key, value in self.editor_theme_data['active-theme']['colors'].items():
+            syntax_label = QLabel(f"{key.capitalize()}")
+            text_color = QColor(value)
+            if key == "font_color":
+                syntax_label.setStyleSheet(f"color: white; background-color: {self.editor_theme_data['active-theme']['colors']['font_color']}; padding: 5px; border-radius: 5px; font-size: 20px; font-family: Fire Code; font-weight: bold;")
+            elif key == "main_color":
+                syntax_label.setStyleSheet(f"color: white; background-color: {self.editor_theme_data['active-theme']['colors']['main_color']}; padding: 5px; border-radius: 5px; font-size: 20px; font-family: Fire Code; font-weight: bold;")
+            else:
+                syntax_label.setStyleSheet(f"color: white; background-color: {self.editor_theme_data['active-theme']['colors']['secondary_color']};padding: 5px; border-radius: 5px; font-size: 20px; font-family: Fire Code; font-weight: bold;")
+            layout.addWidget(syntax_label)
+        
+        
         self.setLayout(layout)
 
     def refresh_style(self, name):
@@ -606,6 +667,16 @@ class ThemeEditor(FramelessMainWindow):
         
         main_theme_list = QListWidget()
         main_theme_list.setStyleSheet(self.refresh_style('themeEditorList'))
+        separator = QListWidgetItem("Theme Overview")
+        separator.setFlags(separator.flags() & ~Qt.ItemIsSelectable)
+        separator.setFlags(separator.flags() & ~Qt.ItemIsEnabled) 
+        font = QFont()
+        font.setPointSize(20)
+        separator.setFont(font)
+        separator.setForeground(QColor("#ffffff"))
+        separator.setTextAlignment(Qt.AlignCenter)
+        main_theme_list.addItem(separator)
+        main_theme_list.addItem("Current Theme")
         separator0 = QListWidgetItem("Lexer")
         separator0.setFlags(separator0.flags() & ~Qt.ItemIsSelectable)
         separator0.setFlags(separator0.flags() & ~Qt.ItemIsEnabled) 
@@ -632,6 +703,8 @@ class ThemeEditor(FramelessMainWindow):
         main_theme_list.addItem("Browse Editor Themes")
 
         stack = QStackedWidget()
+        stack.addWidget(QLabel("Theme Overview"))
+        stack.addWidget(ThemeInfoWidget(lexer_theme_data=gsedit.theme_editor.read_theme_file(), editor_theme_data=gsedit.theme_editor.read_editor_theme_file(), parent=self.mwindow))
         stack.addWidget(LexerThemeInfoWidget(theme_data=gsedit.theme_editor.read_theme_file()))
         stack.addWidget(LexerThemeInfoWidget(theme_data=gsedit.theme_editor.read_theme_file()))
         stack.addWidget(LexerEditorWidget(theme_data=gsedit.theme_editor.read_theme_file(), parent=self.mwindow, brother=self))
